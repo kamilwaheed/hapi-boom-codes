598074508c8d62b4f948c36f3ceadbc1c129edf9
fawadbadar <fawadbadar@gmail.com>
2018-11-06 11:37:12 +0500

    Hapi-17 support

--- test/hapi-response-errors_test.js
+++ test/hapi-response-errors_test.js
@@ -1,102 +1,72 @@
+/* eslint-env es6 */
 'use strict';
 
-var hapi = require('hapi')
-  , should = require('should')
-  , Boom = require('boom')
-  , plugin = require('../');
+var hapi = require('hapi'),
+  Boom = require('boom'),
+  plugin = require('../'),
+  should = require('should');
 
-describe('hapi-response-errors', function() {
+describe('hapi-response-errors', function () {
 
   var server;
   var errorCodes = { InvalidEmail: 'InvalidEmail' };
 
   // prepare test environment
   before(function () {
-    server = new hapi.Server();
-    server.connection({port: 3000});
+    server = new hapi.Server({ port: 3000 });
     server.route({
       method: 'GET',
       path: '/',
-      handler: function(request, reply) {
-        return reply(
-          Boom.badRequest('The email address doesn\'t look good.', { code: errorCodes.InvalidEmail })
-        );
+      handler: () => {
+        return Boom.badRequest('The email address doesn\'t look good.', { code: errorCodes.InvalidEmail });
       }
     });
   });
 
   // destroy test environment
-  after(function(done) {
-    server.stop(function() {
-      done();
-    });
+  after(async () => {
+    await server.stop();
   });
 
-  it('loads successfully', function(done) {
-    server.register(plugin, function(err) {
-
-      (err === undefined || err === null).should.be.true;
-
-      server.start(function(err) {
-        if (err) {
-          throw err;
-        }
-
-        done();
-      });
-
-    });
+  it('loads successfully', async () => {
+    await server.register({ plugin });
+    await server.start();
   });
 
-  it('should include the `code` field in the response', function(done) {
-    server.inject('/', function(res) {
-      var result = res.result;
+  it('should include the `code` field in the response', async () => {
+    const res = await server.inject('/');
+    var result = res.result;
+    result.should.have.property('statusCode');
+    result.statusCode.should.equal(400);
 
-      result.should.have.property('statusCode');
-      result.statusCode.should.equal(400);
+    result.should.have.property('error');
+    result.error.should.equal('Bad Request');
 
-      result.should.have.property('error');
-      result.error.should.equal('Bad Request');
-
-      result.should.have.property('code');
-      result.code.should.equal(errorCodes.InvalidEmail);
-
-      done();
-    });
+    result.should.have.property('code');
+    result.code.should.equal(errorCodes.InvalidEmail);
   });
 
   describe('multiple onPreResponse handlers', function () {
     var calls = 0;
 
     // add a second onPreResponse handler
-    before(function (done) {
+    before(async () => {
       var testPlugin = {
-        register: function (server, options, next) {
-          server.ext('onPreResponse', function(request, reply) {
+        register: function (server) {
+          server.ext('onPreResponse', function (request, h) {
             calls += 1;
-            return reply.continue();
+            return h.continue;
           });
-
-          return next();
         }
       };
-      testPlugin.register.attributes = { name: 'test' };
+      testPlugin.name = 'test';
 
-      server.register(testPlugin, function (err) {
-        if (err) {
-          throw err;
-        }
-
-        done();
-      })
+      await server.register(testPlugin);
     });
 
-    it('should call the second plugin', function (done) {
-      server.inject('/', function(res) {
-        calls.should.equal(1);
-
-        done();
-      });
+    it('should call the second plugin', async () => {
+      await server.inject('/');
+      calls.should.equal(1);
     });
   });
 });
